76664fd3b5082d531d76ad5ffa0cae419c496373
taurasbear <youngicorn@gmail.com>
2024-10-18 17:29:37 +0300

    added booktypes to seeding

--- PCElibrary.Infrastructure/DbContext/ModelBuilderExtensions.cs
+++ PCElibrary.Infrastructure/DbContext/ModelBuilderExtensions.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.EntityFrameworkCore;
     using PCElibrary.Domain.Entities;
+    using PCElibrary.Domain.Enums;
 
     public static class ModelBuilderExtensions
     {
@@ -10,6 +11,11 @@
             modelBuilder.Entity<Book>().HasData(
                 new Book { Id = 1, Title = "1984", Year = 1954, ImagePath = "/images/1984-george-orwell.jpg" },
                 new Book { Id = 2, Title = "To Kill a Mockingbird", Year = 1930, ImagePath = "/images/to-kill-a-mocking-bird.jpg" }
+            );
+
+            modelBuilder.Entity<BookType>().HasData(
+                new BookType { Id = 1, Format = BookFormat.Physical, BookId = 1 },
+                new BookType { Id = 2, Format = BookFormat.Audio, BookId = 2 }
             );
         }
     }
