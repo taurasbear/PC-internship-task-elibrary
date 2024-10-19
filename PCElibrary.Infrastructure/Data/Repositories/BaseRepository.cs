1e33b67537452c2c647fe8a5cfb6e8630b3dd1dc
taurasbear <youngicorn@gmail.com>
2024-10-19 13:56:47 +0300

    quality issues

--- PCElibrary.Infrastructure/Data/Repositories/BaseRepository.cs
+++ PCElibrary.Infrastructure/Data/Repositories/BaseRepository.cs
@@ -1,12 +1,7 @@
-﻿using PCElibrary.Infrastructure.Data.DbContext;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PCElibrary.Infrastructure.Data.Repositories
+﻿namespace PCElibrary.Infrastructure.Data.Repositories
 {
+    using PCElibrary.Infrastructure.Data.DbContext;
+
     public class BaseRepository
     {
         protected readonly LibraryContext libraryContext;
